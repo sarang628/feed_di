ad8eee3e28d6b7a5fb2be377fff0aeaca1fe285a
sryang <sarang628@gmail.com>
2024-05-02 16:37:53 +0800

    migration feeds

--- FeedScreen.kt
+++ FeedScreen.kt
@@ -1,9 +1,14 @@
 package com.sarang.torang.di.feed_di
 
-import android.util.Log
 import androidx.compose.runtime.Composable
-import com.sarang.torang.compose.feed.MainFeedScreen
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import com.sarang.torang.compose.feed.Feed
 import com.sarang.torang.compose.feed.Feeds
+import com.sarang.torang.compose.feed.MainFeedScreen
 import com.sarang.torang.uistate.FeedUiState
 import com.sarang.torang.uistate.FeedUiState.Error
 import com.sarang.torang.uistate.FeedUiState.Success
@@ -12,48 +17,17 @@ import com.sarang.torang.uistate.FeedsUiState
 @Composable
 fun ProvideFeedScreen(
     onAddReview: (() -> Unit),
+    progressTintColor: Color? = null,
 ) {
-    val TAG = "_ProvideFeedScreen"
+    var scrollEnabled by remember { mutableStateOf(true) }
     MainFeedScreen(
         onAddReview = onAddReview,
-        feeds = { feedUiState, onRefresh, onBottom, isRefreshing ->
-            when (feedUiState) {
-                is FeedUiState.Loading -> {
-                    Feeds(
-                        onRefresh = onRefresh,
-                        onBottom = onBottom,
-                        isRefreshing = isRefreshing,
-                        feedsUiState = FeedsUiState.Loading
-                    )
-                }
-
-                is Success -> {
-                    Feeds(
-                        onRefresh = onRefresh,
-                        onBottom = onBottom,
-                        isRefreshing = isRefreshing,
-                        feedsUiState = FeedsUiState.Success(
-                            feedUiState.list.map {
-                                it.review(
-                                    onProfile = { Log.d(TAG, "onProfile()") },
-                                    onImage = { Log.d(TAG, "onImage()") },
-                                    onRestaurant = { Log.d(TAG, "onRestaurant()") },
-                                    onName = { Log.d(TAG, "onName()") },
-                                    onMenu = { Log.d(TAG, "onMenu()") },
-                                    onShare = { Log.d(TAG, "onShare()") },
-                                    onComment = { Log.d(TAG, "onComment()") }
-                                )
-                            }
-                        )
-                    )
-                }
-
-                is Error -> {
-
-                }
-
-                else -> {}
-            }
+        feed = {
+            Feed(
+                review = it.review { },
+                isZooming = { scrollEnabled = !it },
+                progressTintColor = progressTintColor
+            )
         }
     )
 }
