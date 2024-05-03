66241611da99221a5f992e1479a51efba9de50e8
sryang <sarang628@gmail.com>
2024-05-03 12:59:01 +0800

    optimize

--- FeedScreen.kt
+++ FeedScreen.kt
@@ -1,49 +1,43 @@
 package com.sarang.torang.di.feed_di
 
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import com.sarang.torang.compose.feed.Feed
-import com.sarang.torang.compose.feed.Feeds
 import com.sarang.torang.compose.feed.MainFeedScreen
-import com.sarang.torang.uistate.FeedUiState
-import com.sarang.torang.uistate.FeedUiState.Error
-import com.sarang.torang.uistate.FeedUiState.Success
-import com.sarang.torang.uistate.FeedsUiState
 
-@Composable
-fun ProvideFeedScreen(
-    onAddReview: (() -> Unit),
+fun provideFeedScreen(
+    navController: NavHostController,
     progressTintColor: Color? = null,
-    onComment: ((Int) -> Unit)? = null,
-    onShare: ((Int) -> Unit)? = null,
-    onMenu: ((Int) -> Unit)? = null,
-    onName: ((Int) -> Unit)? = null,
-    onRestaurant: ((Int) -> Unit)? = null,
     onImage: ((Int) -> Unit)? = null,
-    onProfile: ((Int) -> Unit)? = null,
-) {
-    var scrollEnabled by remember { mutableStateOf(true) }
-    MainFeedScreen(
-        onAddReview = onAddReview,
-        feed = {
-            Feed(
-                review = it.review(
-                    onComment = {onComment?.invoke(it.reviewId)},
-                    onShare = { onShare?.invoke(it.reviewId) },
-                    onMenu = { onMenu?.invoke(it.reviewId) },
-                    onName = { onName?.invoke(it.userId) },
-                    onRestaurant = {onRestaurant?.invoke(it.restaurantId)},
-                    onImage = onImage,
-                    onProfile = { onProfile?.invoke(it.userId) }
-                ),
-                isZooming = { scrollEnabled = !it },
-                progressTintColor = progressTintColor
-            )
-        }
-    )
-}
+    onShowComment: () -> Unit,
+): @Composable (onComment: ((Int) -> Unit), onMenu: ((Int) -> Unit), onShare: ((Int) -> Unit)) -> Unit =
+    { onComment, onMenu, onShare ->
+        var scrollEnabled by remember { mutableStateOf(true) }
+        MainFeedScreen(
+            onAddReview = { navController.navigate("addReview") },
+            feed = {
+                Feed(
+                    review = it.review(
+                        onComment = {
+                            onComment.invoke(it.reviewId)
+                            onShowComment.invoke()
+                        },
+                        onShare = { onShare.invoke(it.reviewId) },
+                        onMenu = { onMenu.invoke(it.reviewId) },
+                        onName = { navController.navigate("profile/${it.userId}") },
+                        onRestaurant = { navController.navigate("restaurant/${it.restaurantId}") },
+                        onImage = onImage,
+                        onProfile = { navController.navigate("profile/${it.userId}") }
+                    ),
+                    isZooming = { scrollEnabled = !it },
+                    progressTintColor = progressTintColor
+                )
+            }
+        )
+    }
