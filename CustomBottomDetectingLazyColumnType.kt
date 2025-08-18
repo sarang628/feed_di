f524cc0da16156371588ece8d3fa849ffa73d480
sryang <sarang628@gmail.com>
2025-08-18 14:06:33 +0800

    refactor : add bottomDetectingLazyColumnType

---
+++ CustomBottomDetectingLazyColumnType.kt
@@ -0,0 +1,5 @@
+package com.sarang.torang.di.feed_di
+
+import com.sarang.torang.compose.feed.component.bottomDetectingLazyColumnType
+
+val CustomBottomDetectingLazyColumnType: bottomDetectingLazyColumnType = provideBottomDetectingLazyColumn()
