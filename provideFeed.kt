263b9955e494a3a800eee321a22e4a3081ec8179
sryang <sarang628@gmail.com>
2025-08-20 15:35:09 +0800

    refactor : remove empty parameter

--- provideFeed.kt
+++ provideFeed.kt
@@ -1,42 +1,21 @@
 package com.sarang.torang.di.feed_di
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.sarang.torang.VideoPlayerScreen
 import com.sarang.torang.compose.feed.Feed
 import com.sarang.torang.compose.feed.feedType
 import com.sarang.torang.di.image.provideZoomableTorangAsyncImage
 import com.sarang.torang.provideExpandableText
 
-fun provideFeed(imageLoadCompose: @Composable (Modifier, String, Dp?, Dp?, ContentScale?, Dp?) -> Unit = provideZoomableTorangAsyncImage()): feedType =
+fun provideFeed(): feedType =
     { feed, onLike, onFavorite, isLogin, onVideoClick, imageHeight, pageScrollAble ->
         Feed(
             review = feed.toReview(),
-            imageLoadCompose = imageLoadCompose,
-            onMenu = {},
+            imageLoadCompose = provideZoomableTorangAsyncImage(),
             onLike = { onLike.invoke(feed.reviewId) },
             onFavorite = { onFavorite.invoke(feed.reviewId) },
-            onComment = {},
-            onShare = {},
-            onProfile = {},
-            isZooming = {},
-            onName = {},
-            onImage = {},
-            onRestaurant = {},
-            onLikes = {},
             isLogin = isLogin,
             expandableText = provideExpandableText(),
-            videoPlayer = {
-                VideoPlayerScreen(
-                    videoUrl = it,
-                    isPlaying = feed.isPlaying,
-                    onClick = onVideoClick,
-                    onPlay = {}
-                )
-            },
+            videoPlayer = { VideoPlayerScreen(videoUrl = it, isPlaying = feed.isPlaying, onClick = onVideoClick, onPlay = {}) },
             imageHeight = if (imageHeight != 0) imageHeight.dp else 400.dp,
             pageScrollAble = pageScrollAble
         )
