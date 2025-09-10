78d2fcf891d84e2ca6aa787da14764a1e142d3e7
sryang <sarang628@gmail.com>
2025-09-10 14:56:20 +0800

    fix remove top space

--- CustomPullToRefreshType.kt
+++ CustomPullToRefreshType.kt
@@ -1,9 +1,11 @@
 package com.sarang.torang.di.feed_di
 
+import androidx.compose.ui.Modifier
 import com.sarang.torang.compose.feed.pullToRefreshLayoutType
 import com.sarang.torang.di.pulltorefresh.providePullToRefresh
 import com.sryang.library.pullrefresh.rememberPullToRefreshState
 
 val CustomPullToRefreshType: pullToRefreshLayoutType = { modifier, isRefreshing, onRefresh, contents ->
-    providePullToRefresh(rememberPullToRefreshState()).invoke(modifier, isRefreshing, onRefresh, contents)
+    // FeedScreen에 Scaffold가 있어 modifier을 설정하면 위에 공간이 생김.
+    providePullToRefresh(rememberPullToRefreshState()).invoke(Modifier, isRefreshing, onRefresh, contents)
 }
