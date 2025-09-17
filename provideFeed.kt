ad64df18f5261db351a495bd582b01887f0c8b14
sryang <sarang628@gmail.com>
2025-09-17 09:04:40 +0800

    change callback parameter to data class

--- provideFeed.kt
+++ provideFeed.kt
@@ -1,14 +1,17 @@
 package com.sarang.torang.di.feed_di
 
 import com.sarang.torang.compose.feed.FeedItem
+import com.sarang.torang.compose.feed.FeedItemClickEvents
 import com.sarang.torang.compose.feed.feedType
 
 fun provideFeed(): feedType =
     { feed, onLike, onFavorite, isLogin, onVideoClick, imageHeight, pageScrollAble ->
         FeedItem(
             uiState = feed.toReview().copy(isLogin = isLogin),
-            onLike = { onLike.invoke(feed.reviewId) },
-            onFavorite = { onFavorite.invoke(feed.reviewId) },
+            feedItemClickEvents = FeedItemClickEvents(
+                onLike = { onLike.invoke(feed.reviewId) },
+                onFavorite = { onFavorite.invoke(feed.reviewId) },
+            ),
             pageScrollAble = pageScrollAble
         )
     }
