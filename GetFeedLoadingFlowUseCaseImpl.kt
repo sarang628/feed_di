1ec01f8c81bd8512beb80531483f7e46174bd09e
sryang <sarang628@gmail.com>
2025-09-24 08:39:24 +0800

    separate FeedLoadingUiState and FeedUiState
    - when like clicked screen is blinking

--- GetFeedLoadingFlowUseCaseImpl.kt
+++ GetFeedLoadingFlowUseCaseImpl.kt
@@ -2,8 +2,6 @@ package com.sarang.torang.di.feed_di
 
 import com.sarang.torang.repository.FeedRepository
 import com.sarang.torang.uistate.FeedLoadingUiState
-import com.sarang.torang.uistate.FeedUiState
-import com.sarang.torang.usecase.GetFeedFlowUseCase
 import com.sarang.torang.usecase.GetFeedLodingFlowUseCase
 import com.sarang.torang.usecase.IsLoginFlowForFeedUseCase
 import dagger.Module
@@ -13,26 +11,26 @@ import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
 @InstallIn(SingletonComponent::class)
 @Module
-class GetFeedFlowUseCaseImpl {
+class GetFeedLoadingFlowUseCaseImpl {
     @Provides
     fun provideGetFeedFlowUseCase(
         feedRepository: FeedRepository,
-        loginFlowForFeedUseCase: IsLoginFlowForFeedUseCase
-    ): GetFeedFlowUseCase {
-        return object : GetFeedFlowUseCase {
-            override fun invoke(coroutineScope: CoroutineScope): StateFlow<FeedUiState> {
-                return combine(feedRepository.feeds, loginFlowForFeedUseCase.isLogin){ feeds , isLogin ->
-                    FeedUiState(feeds?.map { it.toFeedData() } ?: listOf(), isLogin)
+    ): GetFeedLodingFlowUseCase {
+        return object : GetFeedLodingFlowUseCase {
+            override fun invoke(coroutineScope: CoroutineScope): StateFlow<FeedLoadingUiState> {
+                return feedRepository.feeds.map {
+                    feeds ->
+                    if (feeds == null) FeedLoadingUiState.Loading
+                    else FeedLoadingUiState.Success
                 }.stateIn(
                     scope = coroutineScope,
                     started = SharingStarted.WhileSubscribed(5_000),
-                    initialValue = FeedUiState()
+                    initialValue = FeedLoadingUiState.Loading
                 )
             }
         }
