d7657463b1eb7b8e654c4c2e66eba4124350f2ab
sryang <sarang628@gmail.com>
2025-10-04 13:10:30 +0900

    add feed loding failed

--- provideBottomDetectingLazyColumn.kt
+++ provideBottomDetectingLazyColumn.kt
@@ -1,6 +1,9 @@
 package com.sarang.torang.di.feed_di
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.sarang.torang.compose.feed.type.BottomDetectingLazyColumnData
 import com.sryang.library.BottomDetectingLazyColumn
 
@@ -9,14 +12,16 @@ fun provideBottomDetectingLazyColumn(): @Composable (
     BottomDetectingLazyColumnData
 ) -> Unit {
     return { data ->
-        BottomDetectingLazyColumn(
-            modifier = data.modifier,
-            items = data.count,
-            onBottom = { data.onBottom.invoke() },
-            userScrollEnabled = data.userScrollEnabled,
-            verticalArrangement = data.arrangement,
-            listState = data.listState,
-            contents = { data.itemCompose.invoke(it) }
-        )
+        Box(modifier = data.modifier){
+            BottomDetectingLazyColumn(
+                items = data.count,
+                onBottom = { data.onBottom.invoke() },
+                userScrollEnabled = data.userScrollEnabled,
+                verticalArrangement = data.arrangement,
+                listState = data.listState,
+                contents = { data.itemCompose.invoke(it) }
+            )
+            data.content.invoke()
+        }
     }
 }
