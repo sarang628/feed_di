e9ac50d6b27cf35bfeab2ae7b6c162cdf7d84c92
sryang <sarang628@gmail.com>
2025-12-13 15:39:58 +0900

    fix : bouncing when scroll up while scroll to refresh

--- customPullToRefresh.kt
+++ customPullToRefresh.kt
@@ -1,7 +1,11 @@
 package com.sarang.torang.di.feed_di
 
+import android.util.Log
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
+import com.sarang.torang.compose.feed.state.RefreshIndicatorState
 import com.sarang.torang.compose.feed.type.pullToRefreshLayoutType
 import com.sarang.torang.di.pulltorefresh.PullToRefreshData
 import com.sarang.torang.di.pulltorefresh.providePullToRefresh
@@ -11,8 +15,31 @@ val customPullToRefresh: pullToRefreshLayoutType = { modifier, pullToRefreshLayo
     // FeedScreen에 Scaffold가 있어 modifier을 설정하면 위에 공간이 생김.
 
     val indicatorState by pullToRefreshLayoutState.refreshIndicatorState
+    val state = rememberPullToRefreshState()
 
-    providePullToRefresh(rememberPullToRefreshState()).invoke(
+    LaunchedEffect(Unit) {
+        snapshotFlow {
+            state.refreshIndicatorState.value
+        }.collect {
+            Log.d("__customPullToRefresh", "${it}")
+            when(it){
+                com.sryang.library.pullrefresh.RefreshIndicatorState.Default->{
+                    pullToRefreshLayoutState.updateState(RefreshIndicatorState.Default)
+                }
+                com.sryang.library.pullrefresh.RefreshIndicatorState.Refreshing->{
+                    //pullToRefreshLayoutState.updateState(RefreshIndicatorState.Refreshing)
+                }
+                com.sryang.library.pullrefresh.RefreshIndicatorState.ReachedThreshold->{
+                    pullToRefreshLayoutState.updateState(RefreshIndicatorState.ReachedThreshold)
+                }
+                com.sryang.library.pullrefresh.RefreshIndicatorState.PullingDown->{
+                    pullToRefreshLayoutState.updateState(RefreshIndicatorState.PullingDown)
+                }
+            }
+        }
+    }
+
+    providePullToRefresh(state).invoke(
         PullToRefreshData(modifier   = modifier,
                                state      = indicatorState ,
                                onRefresh  = onRefresh,
