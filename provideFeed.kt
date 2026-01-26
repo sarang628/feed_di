b2a438a7c47511987715314379343ebd489cec60
sryang <sarang628@gmail.com>
2026-01-26 16:03:34 +0900

    change video player

--- provideFeed.kt
+++ provideFeed.kt
@@ -1,10 +1,14 @@
 package com.sarang.torang.di.feed_di
 
+import androidx.compose.runtime.CompositionLocalProvider
 import com.sarang.torang.compose.feed.FeedItem
+import com.sarang.torang.compose.feed.internal.components.type.LocalVideoPlayerType
 import com.sarang.torang.compose.feed.type.feedType
 import com.sarang.torang.data.basefeed.FeedItemClickEvents
+import com.sarang.torang.di.basefeed_di.CustomVideoPlayerType
 
 fun provideFeed(): feedType = {
+    CompositionLocalProvider(LocalVideoPlayerType provides CustomVideoPlayerType) {
         FeedItem(
             uiState = it.feed.toReview(it.isLogin),
             feedItemClickEvents = FeedItemClickEvents(
@@ -12,6 +16,8 @@ fun provideFeed(): feedType = {
                 onFavorite = { it.onFavorite.invoke(it.feed.reviewId) },
             ),
             pageScroll = it.pageScrollable,
-            onPage = {}
+            onPage = {},
+            isPlaying = it.isPlaying
         )
     }
+}
