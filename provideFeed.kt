42e66422e8a6e7d78c19006df35848463723997b
sryang <sarang628@gmail.com>
2026-01-31 12:24:06 +0900

    when scrolling feed video keep playing which is already loaded

--- provideFeed.kt
+++ provideFeed.kt
@@ -1,6 +1,10 @@
 package com.sarang.torang.di.feed_di
 
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.sarang.torang.compose.feed.FeedItem
 import com.sarang.torang.compose.feed.internal.components.type.LocalVideoPlayerType
 import com.sarang.torang.compose.feed.type.feedType
@@ -8,16 +12,15 @@ import com.sarang.torang.data.basefeed.FeedItemClickEvents
 import com.sarang.torang.di.basefeed_di.CustomVideoPlayerType
 
 fun provideFeed(): feedType = {
-    CompositionLocalProvider(LocalVideoPlayerType provides CustomVideoPlayerType()) {
+    var isPlayed by remember { mutableStateOf(false) }
+    CompositionLocalProvider(LocalVideoPlayerType provides CustomVideoPlayerType(onPlayed = { isPlayed = true })) {
         FeedItem(
-            uiState = it.feed.toReview(it.isLogin),
-            feedItemClickEvents = FeedItemClickEvents(
-                onLike = { it.onLike.invoke(it.feed.reviewId) },
-                onFavorite = { it.onFavorite.invoke(it.feed.reviewId) },
-            ),
-            pageScroll = it.pageScrollable,
-            onPage = {},
-            isPlaying = it.isPlaying
+            uiState             = it.feed.toReview(it.isLogin),
+            pageScroll          = it.pageScrollable,
+            onPage              = {},
+            isPlaying           = it.isPlaying || isPlayed,
+            feedItemClickEvents = FeedItemClickEvents(  onLike      = { it.onLike.invoke(it.feed.reviewId) },
+                                                        onFavorite  = { it.onFavorite.invoke(it.feed.reviewId) } )
         )
     }
 }
